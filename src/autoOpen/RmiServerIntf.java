2a87c47d3e3d7adabe9e36b395a1f89845f3497d
Hung Pei Hsuan <afcidk@gmail.com>
2018-05-28 16:57:41 +0800

    Change the place where time stores
    The function GetRemainTime will obtain the time from
    screenLocker.MyTimer. This class has not been import to now yet.

--- src/autoOpen/RmiServerIntf.java
+++ src/autoOpen/RmiServerIntf.java
@@ -4,5 +4,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RmiServerIntf extends Remote{
-	public long RMI_getRemainTime() throws RemoteException;
+	public int GetRemainTime() throws RemoteException;
 }
