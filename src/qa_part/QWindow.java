6fd662cf4840a6b1f025e3fdcc5485779732bac6
cyj407 <dfghj345678@gmail.com>
2018-06-01 11:59:31 +0800

    add QWindow and remove MainTest

--- src/qa_part/QWindow.java
+++ src/qa_part/QWindow.java
@@ -1,37 +1,82 @@
 package qa_part;
 
-import java.awt.AlphaComposite;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.image.BufferedImage;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class QWindow {
+public class QWindow extends Application {
+	private Question _chosenQuestion = new Question();
+	private GridPane _layout = new GridPane();
+//	private Pane _layout = new Pane();
+	private Button choice_a, choice_b, choice_c, choice_d;
+	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setSize(800,548);
-		frame.setResizable(false);				// set fixed window size
-		frame.setLocationRelativeTo(null);		// put frame in the middle of the screen
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		launch(args);
+	}
+	
+	public void start(Stage questionStage) throws Exception {
+		questionStage.setTitle("Screen Locker");
 		
-		JPanel q_window = new JPanel();
-        q_window.setLayout(new GridBagLayout());
-		q_window.setSize(800,548);
-		ImageIcon question_bg = new ImageIcon(".\\src\\qa_part\\_backgroundQuestion.png");
-		q_window.setOpaque(true);  
+		_layout.setAlignment(Pos.CENTER);
+		_layout.setHgap(10);	// horizontal distance
+		_layout.setVgap(10);	// vertical distance
+		_layout.setPadding(new Insets(50, 50, 50, 50));		// fill the border
 		
-		JLabel label = new JLabel(question_bg);
-		label.setOpaque(true);
-		q_window.add(label);
+		Scene scene = new Scene(_layout, 800, 500);
+		questionStage.setScene(scene);
 		
-		frame.add(q_window);
-		frame.setVisible(true);
+		Text question = new Text(_chosenQuestion.getqn());
+		question.setWrappingWidth(700); 
+		  
+		question.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        _layout.add(question, 0, 0, 2, 2);
+
+        choice_a = new Button();
+		choice_b = new Button();
+		choice_c = new Button();
+		choice_d = new Button();
+		
+		SetButtonFormat(choice_a);
+		SetButtonFormat(choice_b);
+		SetButtonFormat(choice_c);
+		SetButtonFormat(choice_d);
+		
+		SetButtonContent();
+		
+	//	button.setWrapText(true);
+		
+		_layout.add(choice_a,1,2);
+		_layout.add(choice_b,2,2);
+		_layout.add(choice_c,1,3);
+		_layout.add(choice_d,2,3);
+
+		questionStage.show();
+	}
+	
+	public void SetButtonFormat(Button btn) {
+		btn.setMaxSize(150, 80);
+		btn.setStyle("-fx-background-color: #FFFFFF;"
+				+ "-fx-font-size: 1.5em;"
+				+ "-fx-background-radius: 80;");
+	//	btn.setPrefSize(120, 40);
+	}
+	
+	public void SetButtonContent() {
+		choice_a.setText("(A) " + _chosenQuestion.getA());
+		choice_b.setText("(B) " + _chosenQuestion.getB());
+		choice_c.setText("(C) " + _chosenQuestion.getC());
+		choice_d.setText("(D) " + _chosenQuestion.getD());
 	}
 }
